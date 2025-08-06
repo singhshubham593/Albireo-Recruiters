66f6c07188d68e71c241b16f309c5624b8267746
shubhamurl <rajputshubhamsingh593@gmail.com>
2025-08-06 19:55:50 +0530

    Update TeamMember.jsx

--- src/components/Home/TeamMember.jsx
+++ src/components/Home/TeamMember.jsx
@@ -9,7 +9,7 @@ import Hansika from "../../assets/Hansika.jpg";
 import JaganmohanSharma from "../../assets/JagmohanSharma.jpg";
 import MonikaSharma from "../../assets/MonikaSharma.jpg";
 import NancyChawla from "../../assets/NancyChawla.jpg";
-import Rohit from "../../assets/Rohit.jpg";
+// import Rohit from "../../assets/Rohit.jpg";
 import Sangmitra from "../../assets/Sangmitra.jpg";
 import BhavnaTyagi from "../../assets/BhavnaTyagi.jpg";
 import VandniTyagi from "../../assets/VandniTyagi.jpg";
@@ -67,7 +67,7 @@ const people = [
   {
     name: "Vikram Mehra",
     role: "Marketing Head",
-    image:  Rohit,
+    image:  Sangmitra,
   },
   {
     name: "Vikram Mehra",
