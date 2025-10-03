adb2db13c4365e6b962145e0d8768381767875f0
shubhamurl <rajputshubhamsingh593@gmail.com>
2025-10-03 19:14:00 +0530

    Create jobFromdata.js

---
+++ src/api/jobFromdata.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api=axios.create({
+    
+    baseURL: "http://localhost:8000/jobs",
+    headers:{
+        "content-Type": "application/json",
+    },
+});
+api.interceptors.request.use(config => {
+  console.log("🔍 Axios Request:", config.method.toUpperCase(), config.url);
+  return config;
+});
+
+export default api;
